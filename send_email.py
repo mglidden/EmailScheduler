72fa6b786f3fa1b66bb2936cdcdcf854483215b4
Mason Glidden <mglidden@yelp.com>
2013-11-07 20:16:34 -0500

    Small tweaks to the sending script

--- send_email.py
+++ send_email.py
@@ -8,10 +8,15 @@ def is_today(date_string):
   return date.today().month == int(month) and date.today().day == int(day)
 
 def send_email(receiver, config):
+  def construct_address():
+	if '@' in receiver:
+	  return receiver
+	else:
+	  return receiver + config['receiver_domain_name']
   server = smtplib.SMTP(config['mail_server'])
   server.starttls()
   server.login(config['username'], config['password'])
-  server.sendmail(config['from_email'], receiver + config['receiver_domain_name'], 'test message')
+  server.sendmail(config['from_email'], construct_address(), 'Subject: %s\n\n%s' % (config['msg_subject'], config['msg_text']))
   server.quit()
 
 config = json.loads(open('CONFIG.private').read())
